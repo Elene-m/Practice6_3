fee67d5b415603e46c723a4235c4d6e751bd719e
Elene <Elene@ELEMAISA>
2025-02-09 13:58:03 +0400

    Add project files.

---
+++ Practice6_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentProfessorTest.Test();
+        }
+    }
+}
---
+++ Practice6_3/StudentProfessorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice6_3
+{
+    internal class StudentProfessorTest
+    {
+        public static void Test()
+        {
+            Person person = new Person();
+            person.Greet();
+
+            Student student = new Student();
+            student.SetAge(20);
+            student.Greet();
+            student.ShowAge();
+            student.Study();
+
+            Professor professor = new Professor();
+            professor.SetAge(45);
+            professor.Greet();
+            professor.Explain();
+        }
+    }
+}
